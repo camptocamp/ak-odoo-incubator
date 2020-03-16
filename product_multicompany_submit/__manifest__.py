167f769bc9952daa090863aa6fb0faae815a63fb
kevinkhao <kevin.khao@akretion.com.br>
2020-03-16 03:51:48 +0100

    [ADD] product_multicompany_submit: Add to 12.0

---
+++ product_multicompany_submit/__manifest__.py
@@ -0,0 +1,21 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Multicompany submittal workflows",
+    "summary": "Base building block for multicompany submittal workflows",
+    "license": "AGPL-3",
+    "version": "12.0.1.0.0",
+    "author": "Akretion",
+    "maintainer": "Akretion",
+    "category": "Sales",
+    "depends": ["base", "base_multicompany_submit", "sale_management"],
+    "data": [
+        "security/groups.xml",
+        "data/ir_rule.xml",
+        "views/product_approval.xml",
+        "security/ir.model.access.csv",
+    ],
+    "maintainers": ["kevinkhao", "sebastienbeau"],
+    "website": "http://www.akretion.com/",
+    "installable": True,
+}
